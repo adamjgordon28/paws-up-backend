bbd7d59a8d0525851e58489ffcae495dd6af055d
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-02 16:31:32 -0400

    seeded with pets guardian and adopters

--- paws-up/db/seeds.rb
+++ paws-up/db/seeds.rb
@@ -1,2 +1,62 @@
+Adopter.create(name: "Nicky", age: 27, location: "New York City", residence_type: "Apt", allergy: false, pet: true, img_url: "https://media.licdn.com/dms/image/C4D03AQGa0yq77D29og/profile-displayphoto-shrink_800_800/0?e=1562198400&v=beta&t=-r01afowD-gP2wPUgrObwb2Dxi97-wizVLMBRqGvQ0c")
 
-Meeting.create(adopter_id: 2, pet_id: 3, datetime: DateTime.now, location: "New York City" )
+
+Adopter.create(name: "Ryan", age: 52, location: "Queens", residence_type: "Apt", allergy: false, pet: true, img_url: "https://d3ewd3ysu1dfsj.cloudfront.net/images/stories/large/40323.jpg?1507654168")
+
+
+Adopter.create(name: "Matthew", age:30, location: "New Orleans", residence_type: "Apt", allergy: false, pet: true, img_url: "https://m.media-amazon.com/images/M/MV5BMTg0MDc3ODUwOV5BMl5BanBnXkFtZTcwMTk2NjY4Nw@@._V1_.jpg")
+
+
+Adopter.create(name: "Khaled", age: 35, location: "Brooklyn", residence_type: "Apt", allergy: false, pet: true, img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Khalid-MTV_smiling.jpg/220px-Khalid-MTV_smiling.jpg")
+
+
+Adopter.create(name: "Suni", age: 23, location: "Jersey City", residence_type: "Apt", allergy: false, pet: true, img_url: "https://ksassets.timeincuk.net/wp/uploads/sites/55/2016/10/ReyDaisyRidleyStarWars.jpg")
+
+
+
+
+
+Guardian.create(name: "Adam", location: "New York City", img_url: "https://media.licdn.com/dms/image/C4E03AQHld5thf8aJ1g/profile-displayphoto-shrink_200_200/0?e=1562198400&v=beta&t=-cydYNcgLXi0QvuyF86lq3UzfOEnddazoZ8fERW6UlU")
+
+Guardian.create(name: "Kate", location: "Queens", img_url: "https://pmcwwd.files.wordpress.com/2017/01/emma-stone.jpg?w=640&h=415&crop=1")
+
+Guardian.create(name: "Vicky", location: "Miami", img_url: "https://ichef.bbci.co.uk/news/660/cpsprodpb/8703/production/_103036543_gettyimages-950667896.jpg")
+
+Guardian.create(name: "Tali", location: "LA", img_url: "https://nsuspartanecho.files.wordpress.com/2017/06/ap17147262695450.jpg")
+
+
+Guardian.create(name: "Diana", location: "Brooklyn", img_url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU1MzMxMzc0NjA3/84th-annual-academy-awards---arrivals.jpg")
+
+Guardian.create(name: "Adam", location: "New York City", img_url: "https://media.licdn.com/dms/image/C4E03AQHld5thf8aJ1g/profile-displayphoto-shrink_200_200/0?e=1562198400&v=beta&t=-cydYNcgLXi0QvuyF86lq3UzfOEnddazoZ8fERW6UlU")
+
+Guardian.create(name: "Kate", location: "Queens", img_url: "https://pmcwwd.files.wordpress.com/2017/01/emma-stone.jpg?w=640&h=415&crop=1")
+
+Guardian.create(name: "Vicky", location: "Miami", img_url: "https://ichef.bbci.co.uk/news/660/cpsprodpb/8703/production/_103036543_gettyimages-950667896.jpg")
+
+Guardian.create(name: "Tali", location: "LA", img_url: "https://nsuspartanecho.files.wordpress.com/2017/06/ap17147262695450.jpg")
+
+
+Guardian.create(name: "Diana", location: "Brooklyn", img_url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU1MzMxMzc0NjA3/84th-annual-academy-awards---arrivals.jpg")
+
+
+
+Pet.create(name: "Max", sex: "male", guardian_id: 1, animal: "dog", age: 20, location: "New York City", allergy: false, size: "medium", img_url: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn")
+
+Pet.create(name:"Ella", sex: "female", guardian_id: 2, animal: "dog", age: 6, location: "New York City", allergy: false, size: "Medium", img_url: "https://assets.marthastewart.com/styles/wmax-570/d50/chocolate-labrador-dog-puppy-rubber-duck-503937138/chocolate-labrador-dog-puppy-rubber-duck-503937138_sq.jpg?itok=BKTiHoCu")
+
+Pet.create(name:"Eli", sex: "male", guardian_id: 2, animal: "dog", age: 60, location: "New York City", allergy: true, size: "Medium", img_url: "https://pet-uploads.adoptapet.com/3/4/6/361996712.jpg")
+
+
+Pet.create(name:"Toni", sex: "male", guardian_id: 3, animal: "dog", age: 52, location: "New York City", allergy: false, size: "Large", img_url: "https://pet-uploads.adoptapet.com/4/3/8/291593434.jpg")
+
+
+Pet.create(name:"Steven", sex: "male", guardian_id: 3, animal: "dog", age: 72, location: "New York City", allergy: false, size: "Large", img_url: "https://pet-uploads.adoptapet.com/3/8/8/297699715.jpg")
+
+
+Pet.create(name:"Ian", sex: "male", guardian_id: 4, animal: "dog", age: 52, location: "Jersey City", allergy: false, size: "Large", img_url: "https://pet-uploads.adoptapet.com/1/f/b/290096799.jpg")
+
+
+Pet.create(name:"Richie", sex: "male", guardian_id: 4, animal: "dog", age: 12, location: "New York City", allergy: false, size: "Toy", img_url: "https://pet-uploads.adoptapet.com/1/e/4/404309673.jpg")
+
+
+Pet.create(name:"Jessy", sex: "female", guardian_id: 5, animal: "dog", age: 25, location: "New York City", allergy: false, size: "Toy", img_url: "https://pet-uploads.adoptapet.com/c/c/3/405476206.jpg")
