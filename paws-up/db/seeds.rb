27e3d62c2af56bbe20b1c66fb2f5667912f03246
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-08 13:31:24 -0400

    ready

--- paws-up/db/seeds.rb
+++ paws-up/db/seeds.rb
@@ -17,21 +17,37 @@ Adopter.create(name: "Suni", age: 23, location: "Boston", residence_type: "Apt",
 
 Pet.create(name: "Max", sex: "male", animal: "dog", age: 20, location: "New York City", allergy: false, size: "medium", img_url: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn")
 
-Pet.create(name:"Ella", sex: "female", animal: "dog", age: 6, location: "New York City", allergy: false, size: "Medium", img_url: "https://assets.marthastewart.com/styles/wmax-570/d50/chocolate-labrador-dog-puppy-rubber-duck-503937138/chocolate-labrador-dog-puppy-rubber-duck-503937138_sq.jpg?itok=BKTiHoCu")
+Pet.create(name:"Ella", sex: "female", animal: "dog", age: 6, location: "New York City", allergy: false, size: "medium", img_url: "https://assets.marthastewart.com/styles/wmax-570/d50/chocolate-labrador-dog-puppy-rubber-duck-503937138/chocolate-labrador-dog-puppy-rubber-duck-503937138_sq.jpg?itok=BKTiHoCu")
 
-Pet.create(name:"Eli", sex: "male",  animal: "dog", age: 60, location: "New York City", allergy: true, size: "Medium", img_url: "https://pet-uploads.adoptapet.com/3/4/6/361996712.jpg")
-
-
-Pet.create(name:"Toni", sex: "male", animal: "cat", age: 52, location: "New York City", allergy: false, size: "Large", img_url: "https://pet-uploads.adoptapet.com/4/3/8/291593434.jpg")
+Pet.create(name:"Eli", sex: "male",  animal: "dog", age: 60, location: "New York City", allergy: true, size: "medium", img_url: "https://pet-uploads.adoptapet.com/3/4/6/361996712.jpg")
 
 
-Pet.create(name:"Steven", sex: "male", animal: "dog", age: 72, location: "New York City", allergy: false, size: "Large", img_url: "https://pet-uploads.adoptapet.com/3/8/8/297699715.jpg")
+Pet.create(name:"Toni", sex: "male", animal: "cat", age: 52, location: "New York City", allergy: false, size: "large", img_url: "https://pet-uploads.adoptapet.com/4/3/8/291593434.jpg")
 
 
-Pet.create(name:"Ian", sex: "male",  animal: "dog", age: 52, location: "Jersey City", allergy: true, size: "Large", img_url: "https://pet-uploads.adoptapet.com/1/f/b/290096799.jpg")
+Pet.create(name:"Steven", sex: "male", animal: "dog", age: 72, location: "New York City", allergy: false, size: "large", img_url: "https://pet-uploads.adoptapet.com/3/8/8/297699715.jpg")
 
 
-Pet.create(name:"Richie", sex: "male",  animal: "dog", age: 12, location: "New York City", allergy: false, size: "Toy", img_url: "https://pet-uploads.adoptapet.com/1/e/4/404309673.jpg")
+Pet.create(name:"Ian", sex: "male",  animal: "dog", age: 52, location: "Jersey City", allergy: true, size: "large", img_url: "https://pet-uploads.adoptapet.com/1/f/b/290096799.jpg")
 
 
-Pet.create(name:"Jessy", sex: "female",  animal: "dog", age: 25, location: "New York City", allergy: false, size: "Toy", img_url: "https://pet-uploads.adoptapet.com/c/c/3/405476206.jpg")
+Pet.create(name:"Richie", sex: "male",  animal: "dog", age: 12, location: "New York City", allergy: false, size: "small", img_url: "https://pet-uploads.adoptapet.com/1/e/4/404309673.jpg")
+
+
+Pet.create(name:"Jessy", sex: "female",  animal: "dog", age: 25, location: "New York City", allergy: false, size: "small", img_url: "https://pet-uploads.adoptapet.com/c/c/3/405476206.jpg")
+
+Pet.create(name:"Lars", sex: "male", animal: "hamster", age: 19, location: "Boston", allergy: false, size: "medium", img_url: "https://media.istockphoto.com/photos/hamster-isolated-picture-id158732331?k=6&m=158732331&s=612x612&w=0&h=RixQxIPOxHCdM3beVaGbpLCq-8RW2XsvcpH1aDMgKro=")
+
+Pet.create(name:"Spenser", sex: "male", animal: "lizard", age: 26, location: "New York City", allergy: false, size: "large", img_url: "https://news.nationalgeographic.com/content/dam/news/2016/01/06/tokay_gecko/01_tokay-gecko.ngsversion.1452103200406.adapt.1900.1.jpg")
+
+Pet.create(name:"Francine", sex: "female", animal: "pig", age: 34, location: "Chicago", allergy: false, size: "smalle", img_url: "https://i.redd.it/ahg5rdrp9vxz.jpg")
+
+Pet.create(name:"Stuart", sex: "male", animal: "cat", age: 42, location: "Boston", allergy: true, size: "medium", img_url: "https://static1.squarespace.com/static/54e8ba93e4b07c3f655b452e/t/56c2a04520c64707756f4267/1493764650017/")
+
+Pet.create(name:"Renley", sex: "male", animal: "cat", age: 60, location: "New York City", allergy: false, size: "large", img_url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cats-best-pets-16-1529003034.jpg")
+
+Pet.create(name:"Georgia", sex: "female", animal: "hamster", age: 31, location: "Boston", allergy: false, size: "medium", img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUq7r1MgTNQQTDsswPaT88V_d_vzMHp-CGSsWzHbAH1Kz4_Uu9eA")
+
+Pet.create(name:"Pumbaa", sex: "female", animal: "pig", age: 6, location: "Los Angeles", allergy: false, size: "medium", img_url: "https://c.stocksy.com/a/1Ak400/z9/1130757.jpg?1551627490")
+
+Pet.create(name: "Reptar", sex: "male", animal: "lizard", age: 49, location: "Los Angeles", allergy: false, size: "large", img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwttrUhdi5aiKla9Q3PYZEj3wldU9LcT4JdEg7EmqZKhvt7VxHxA")
